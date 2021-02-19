608fd3e2ba221922981ce2844600633cd38a43b6
Egor Badmaev <badmaeve2511@gmail.com>
2021-02-20 01:03:31 +0300

    feat: add checking for an equality

--- main.py
+++ main.py
@@ -32,10 +32,22 @@ def check_square():
     return 3
 
 
+def is_number_already_in(number, line):
+    equality = False
+    for i in line:
+        if i == number:
+            equality = True
+    if equality:
+        return True
+    else:
+        return False
+
+
 @eel.expose()
 def main(a):
     global x, y
     print('Первая горизонтальная линия =>', a[0])
+    print(is_number_already_in(0, a[0]))
     for i in range(9):
         print('Первая вертикальная линия: ', a[0][i])
     print(a)

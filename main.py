c68d8d51685e20ed1fa39d48da327e15768fbb57
Egor Badmaev <badmaeve2511@gmail.com>
2021-02-20 00:41:25 +0300

    feat: add ability to print vertical and horizontal lines

--- main.py
+++ main.py
@@ -1,27 +1,55 @@
 import eel
 
 eel.init("")
-eel.start("index.html", size=(1020, 800))
+# eel.start("index.html", size=(1020, 800))
+
+# line1 = list(map(int, input().split()))
+# line2 = list(map(int, input().split()))
+# line3 = list(map(int, input().split()))
+# line4 = list(map(int, input().split()))
+# line5 = list(map(int, input().split()))
+# line6 = list(map(int, input().split()))
+# line7 = list(map(int, input().split()))
+# line8 = list(map(int, input().split()))
+# line9 = list(map(int, input().split()))
+matrix = [list(map(int, input().split())), list(map(int, input().split())), list(map(int, input().split())),
+          list(map(int, input().split())), list(map(int, input().split())), list(map(int, input().split())),
+          list(map(int, input().split())), list(map(int, input().split())), list(map(int, input().split()))]
 
 
-def check_horizontal():
-    # pass
+def check_horizontal(index):
+    global x, y
     return 1
 
 
-def check_vertical():
-    # pass
+def check_vertical(index):
+    global x, y
     return 2
 
 
 def check_square():
-    # pass
+    global x, y
     return 3
 
 
 @eel.expose()
-def main():
-    # pass
-    check_horizontal()
-    check_vertical()
-    check_square()
+def main(a):
+    global x, y
+    print('Первая горизонтальная линия =>', a[0])
+    for i in range(9):
+        print('Первая вертикальная линия: ', a[0][i])
+    print(a)
+
+
+main(matrix)  # 1 2 3 4 5 6 7 8 9
+"""
+1 2 3 4 5 6 7 8 9
+2 3 4 5 6 7 8 9 1
+3 4 5 6 7 8 9 1 2
+4 5 6 7 8 9 1 2 3
+5 6 7 8 9 1 2 3 4
+6 7 8 9 1 2 3 4 5
+7 8 9 1 2 3 4 5 6
+8 9 1 2 3 4 5 6 7
+9 1 2 3 4 5 6 7 8
+"""
